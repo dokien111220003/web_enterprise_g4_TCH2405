e3f07001164cf6e4b8b311559e004eebdc56a107
ledung1910 <130435178+ledung1910@users.noreply.github.com>
2024-03-12 16:06:01 +0700

    sua file conflict

--- be/models/ArticleModel.js
+++ be/models/ArticleModel.js
@@ -5,7 +5,7 @@ const articleSchema = new mongoose.Schema(
         name: { type: String, required: true, unique: true },
         image: { type: String, required: true },
         status: {type: Boolean, default: false},
-        faculty: { type: mongoose.Schema.Types.ObjectId, ref:'Faculty' },
+        faculty: { type: mongoose.Schema.Types.ObjectId, ref:'Faculty', required: true },
         description: { type: String },
     },
     {
