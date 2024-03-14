5ced2ddec39d9c702418abcbc26f263bcad6202f
dokien111220003 <118035034+dokien111220003@users.noreply.github.com>
2024-03-14 14:48:35 +0700

    phan quyen va update 1 so function, update model

--- be/routes/index.js
+++ be/routes/index.js
@@ -1,9 +1,11 @@
 const FacultyRouter = require('./FacultyRouter')
 const ArticleRouter = require('./ArticleRouter')
+const UserRouter = require('./UserRouter')
 
 const routes = (app) => {
     app.use('/api/article', ArticleRouter),
-    app.use('/api/faculty', FacultyRouter)
+    app.use('/api/faculty', FacultyRouter),
+    app.use('/api/user', UserRouter)
 }
 
 module.exports = routes
