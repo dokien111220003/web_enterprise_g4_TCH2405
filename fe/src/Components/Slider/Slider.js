0a98bf65b7225405862567866891122acd46966c
ngocdeaf <noone0559@gmail.com>
2024-03-16 13:53:02 +0700

    auto slide

--- fe/src/Components/Slider/Slider.js
+++ fe/src/Components/Slider/Slider.js
@@ -1,26 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './Slider.css';
 
-const Slider = ({ images }) => {
+const MySlider = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const sliderRef = useRef(null);
 
-  const goToNextSlide = () => {
-    const newIndex = (currentIndex + 1) % images.length;
-    setCurrentIndex(newIndex);
-  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      sliderRef.current.slickNext();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
 
-  const goToPrevSlide = () => {
-    const newIndex = (currentIndex - 1 + images.length) % images.length;
-    setCurrentIndex(newIndex);
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrows: false,
+    autoplaySpeed: 5000,
+    beforeChange: (oldIndex, newIndex) => setCurrentIndex(newIndex)
   };
 
   return (
     <div className="slider">
-      <button onClick={goToPrevSlide}>Previous</button>
-      <img src={images[currentIndex]} alt={`Slide ${currentIndex}`} />
-      <button onClick={goToNextSlide}>Next</button>
+      <Slider {...settings} ref={sliderRef}>
+        {images.map((image, index) => (
+          <div key={index}>
+            <img
+              src={image}
+              alt={`Slide ${index}`}
+              className={index === currentIndex ? "slide current" : "slide"}
+            />
+          </div>
+        ))}
+      </Slider>
+      <button onClick={() => sliderRef.current.slickPrev()}>Previous</button>
+      <button onClick={() => sliderRef.current.slickNext()}>Next</button>
     </div>
   );
 }
 
-export default Slider;
+export default MySlider;
