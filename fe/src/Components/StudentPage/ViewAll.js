339b768f53f5659856423053d2e908ee440137a9
ngocdeaf <noone0559@gmail.com>
2024-04-08 22:28:02 +0700

    kha khá part 2

--- fe/src/Components/StudentPage/ViewAll.js
+++ fe/src/Components/StudentPage/ViewAll.js
@@ -1,27 +1,79 @@
 import React from 'react';
 import './ViewAll.css';
 import img3 from '../Assets/slider3.jpg';
+import search_icon from '../Assets/search.png';
 
 const ViewAll = () => {
 
     return (
         <div className="viewall-container">
-            <div className="viewall-sidebar">
-                <div className="viewall-sidebar-header">
+            <div class="viewall-sidebar">
+                <div class="viewall-sidebar-header">
                     <h1>All Faculty</h1>
                 </div>
-                <div className="viewall-sidebar-content">
-                    <p>Computer Science</p>
-                    <p>Business</p>
-                    <p>Law</p>
-                    <p>Marketing</p>
+                <div class="viewall-sidebar-content">
+                    <a href="#" class="filter-link">Computer Science</a>
+                    <a href="#" class="filter-link">Business</a>
+                    <a href="#" class="filter-link">Law</a>
+                    <a href="#" class="filter-link">Marketing</a>
                 </div>
             </div>
+
             <div className="viewall-content">
                 <div className="viewall-content-header">
                     <h1>All Contribution</h1>
+                    <div className='search-container-viewall'>
+                        <div className="search-input-viewall">
+                            <img src={search_icon} alt="" className="icon-search-viewall" />
+                            <input type="search" placeholder="Search" />
+                        </div>
+                    </div>
                 </div>
                 <div className="viewall-content-contri">
+                    <div className="viewall-box">
+                        <div className="viewall-box-img">
+                            <img src={img3}></img>
+                        </div>
+                        <div className="viewall-content-title">
+                            <h1>The Impact of Social Media on Youth Mental Health</h1>
+                        </div>
+                        <div className="viewall-content-author">
+                            <h3>By Paul Atreides</h3>
+                        </div>
+                    </div>
+                    <div className="viewall-box">
+                        <div className="viewall-box-img">
+                            <img src={img3}></img>
+                        </div>
+                        <div className="viewall-content-title">
+                            <h1>The Impact of Social Media on Youth Mental Health</h1>
+                        </div>
+                        <div className="viewall-content-author">
+                            <h3>By Paul Atreides</h3>
+                        </div>
+                    </div>
+                    <div className="viewall-box">
+                        <div className="viewall-box-img">
+                            <img src={img3}></img>
+                        </div>
+                        <div className="viewall-content-title">
+                            <h1>The Impact of Social Media on Youth Mental Health</h1>
+                        </div>
+                        <div className="viewall-content-author">
+                            <h3>By Paul Atreides</h3>
+                        </div>
+                    </div>
+                    <div className="viewall-box">
+                        <div className="viewall-box-img">
+                            <img src={img3}></img>
+                        </div>
+                        <div className="viewall-content-title">
+                            <h1>The Impact of Social Media on Youth Mental Health</h1>
+                        </div>
+                        <div className="viewall-content-author">
+                            <h3>By Paul Atreides</h3>
+                        </div>
+                    </div>
                     <div className="viewall-box">
                         <div className="viewall-box-img">
                             <img src={img3}></img>
